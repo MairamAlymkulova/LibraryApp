cc20383ac5b2176b621dfc3c2879788cf80c3459
MayramAlymkulova <mayram.a2003@gmail.com>
2023-11-10 14:53:55 +0600

    Initial Commit

---
+++ LibraryApp/LibraryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAppApp.swift
+//  LibraryApp
+//
+//  Created by A Mairam on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LibraryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
